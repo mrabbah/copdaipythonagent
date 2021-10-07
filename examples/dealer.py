346e3137ef9504f3c864c5359b3b956d186eba75
domain <mrabbah@gmail.com>
2021-10-07 03:24:10 +0100

    refractoring test

--- examples/dealer.py
+++ examples/dealer.py
@@ -2,20 +2,34 @@ import time
 import zmq
 import logging
 import os
+import gc
+import atexit
+from functools import wraps
 
-# Main function
-if __name__ == '__main__':
-    logging.basicConfig(format="%(asctime)s: %(message)s", level=logging.INFO, datefmt="%H:%M:%S")
+logging.basicConfig(format="%(asctime)s: %(message)s", level=logging.INFO, datefmt="%H:%M:%S")
+
+
+def gc_decorator(critical_func):
+    @wraps(critical_func)
+    def wrapper(*args, **kwargs):
+        gc.disable()
+        # gc.disable() doesn't work, because some random 3rd-party library will
+        gc.set_threshold(0)
+        # Suicide immediately after other atexit functions finishes.
+        # CPython will do a bunch of cleanups in Py_Finalize which
+        # will again cause Copy-on-Write, including a final GC
+        atexit.register(os._exit, 0)
+        return critical_func(*args, **kwargs)
+
+
+def run():
     logging.info("Dealer Node Started")
-
     root_folder = "/tmp/"
     proto = "ipc://"
     topic_folder = "cdp"
-
     path = os.path.join(root_folder, topic_folder)
     topics_path = os.listdir(path)
     logging.info("Topics list : %s " % topics_path)
-
     topics = []
     context = zmq.Context()
     sockets = []
@@ -25,24 +39,25 @@ if __name__ == '__main__':
         topic = "%s%s%s/%s" % (proto, root_folder, topic_folder, topic_path)
         topics.append(topic)
         socket = context.socket(zmq.DEALER)
+        socket.setsockopt(zmq.LINGER, 0)
+        socket.setsockopt(zmq.SNDTIMEO, 0)
         socket.connect(topic)
         sockets.append(socket)
-
     iterator = 0
     try:
         while True:
             for socket in sockets:
                 time_nanosec = time.clock_gettime_ns(time.CLOCK_MONOTONIC)
-                # msg = "%d,%d" % (iterator, time_nanosec)
-                # b_msg = str.encode(msg)
                 socket.send(b"%d,%d" % (iterator, time_nanosec))
-                # socket.send_string(f"{iterator},{time_nanosec}")
-                # socket.send(b"MSG_%d" % iterator)
-                # socket.send(b_msg)
-                # socket.send(b"Message %d " % iterator)
-                logging.info("Message %d sent at %d " % (iterator, time_nanosec))
+                # logging.info("Message %d sent at %d " % (iterator, time_nanosec))
+                time.sleep(0.01)
             iterator += 1
     except KeyboardInterrupt:
         for socket in sockets:
             socket.close()
         logging.info("Terminating Dealer Node")
+
+
+# Main function
+if __name__ == '__main__':
+    run()
