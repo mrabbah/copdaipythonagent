41461ba66a1187a31ac7d39765b8500846a9d774
domain <mrabbah@gmail.com>
2021-10-07 05:54:54 +0100

    running utilities

---
+++ examples/lunch_routers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Number of nodes to run = $1 "
+
+for i in $(seq "$1"); do
+    ./routers.py "$i" &
+    PID=$(pgrep -f "routers.py $i")
+    echo "router $i process id = $PID"
+    chrt -f -p 99 "$PID"
+done
